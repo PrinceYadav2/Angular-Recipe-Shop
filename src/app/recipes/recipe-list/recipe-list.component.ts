0e33488bbfa4db7613ffc155a2d0824147de3138
Prince Yadav <prince.y@osmosys.co>
2023-04-26 07:30:18 +0530

    Setup core project, Implement various components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('A Test Recipe', 'Test Description', 'https://th.bing.com/th/id/OIP.pdoPE4sCpZ4DPPshT6rn1wHaLH?rs=1'),
+    new Recipe('A Test Recipe 2', 'Test Description', 'https://th.bing.com/th/id/OIP.pdoPE4sCpZ4DPPshT6rn1wHaLH?rs=1'),
+    new Recipe('A Test Recipe 3', 'Test Description', 'https://th.bing.com/th/id/OIP.pdoPE4sCpZ4DPPshT6rn1wHaLH?rs=1')
+  ]
 
   constructor() { }
 
